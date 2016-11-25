8761be061ac563d818f12cc30e9a87fb4d508744
DorianGrey <ShadowVampire@web.de>
2016-11-25 08:38:10 +0100

    Remove core-js client shim from default polyfills => If you need it in your particular circumstance, just add it again.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,9 @@
 import "core-js/es6/reflect";
 import "core-js/es7/reflect";
-import "core-js/client/shim";
+/*
+ Core-js is a module library that provides polyfills for almost every ES6 and ES7 feature.
+ This is the right position to reference every polyfill required in your particular project.
+ */
 import "zone.js/dist/zone";
 import "zone.js/dist/long-stack-trace-zone";
 import "ts-helpers";
