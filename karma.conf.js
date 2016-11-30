9e301e2110bc0afadb73b564fb63d82d512a339f
DorianGrey <ShadowVampire@web.de>
2016-11-30 20:37:19 +0100

    Fixes: - Re-add unit test execution to dev mode - Add production mode root reducer (skips dev specific reducers) - Add documentation for the 'AppState' handling

--- karma.conf.js
+++ karma.conf.js
@@ -1,51 +1,58 @@
 const webpackTestConfig = require("./config/webpack.test.config");
 
-const ENV       = process.env.NODE_ENV || "development";
-const reporters = ["mocha", "coverage", "remap-coverage"];
-if ("production" === ENV) {
-  reporters.push("junit");
-}
+const ENV                = process.env.NODE_ENV || "development";
+const devSpecificConfig  = {
+  // In dev mode, we don't need to see the coverage stuff on every run ...
+  preprocessors: {
+    "./src/test-setup.js": ["webpack", "sourcemap"]
+  },
+};
+const prodSpecificConfig = {
+  // In prod-test mode, we do want to see and store the coverage, thus ...
+  preprocessors: {
+    "./src/test-setup.js": ["coverage", "webpack", "sourcemap"]
+  },
+  coverageReporter: {
+    type: "in-memory"
+  },
+  remapCoverageReporter: {
+    "text-summary": null,
+    json: "./test-results/coverage/coverage.json",
+    html: "./test-results/coverage/html"
+  },
+  // The junit reporter is primarily used to provide a report that may be picked
+  // up be almost every tool that can handle XUnit conforming reports. E.g. CI systems like Jenkins
+  // have several plugins for this part of the process.
+  junitReporter: {
+    outputDir: "./test-results/junit"
+  }
+};
 
 module.exports = config => {
-  const configuration = {
+  const baseConfig = {
     basePath: "",
     frameworks: ["jasmine"],
-
     files: [
       {pattern: "./src/test-setup.js", watched: false}
     ],
     exclude: [],
-    preprocessors: {
-      "./src/test-setup.js": ["coverage", "webpack", "sourcemap"]
-    },
     webpack: webpackTestConfig,
     webpackServer: {noInfo: true},
-
-    reporters: reporters,
-
-    coverageReporter: {
-      type: "in-memory"
-    },
-
-    remapCoverageReporter: {
-      "text-summary": null,
-      json: "./test-results/coverage/coverage.json",
-      html: "./test-results/coverage/html"
-    },
-    junitReporter: {
-      outputDir: "./test-results/junit"
-    },
-
+    // See the comments above for an explanation of this difference.
+    reporters: ENV === "production" ? ["mocha", "coverage", "remap-coverage", "junit"] : ["mocha"],
     mochaReporter: {
       output: "minimal"
     },
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: ENV === "development",
+    // Reduce output noise to a minimum in dev mode, so that the results are easier to keep an eye on.
+    logLevel: ENV === "production" ? config.LOG_INFO : config.LOG_WARN,
+    autoWatch: ENV !== "production",
     browsers: ["PhantomJS"],
     singleRun: ENV === "production"
   };
 
-  config.set(configuration);
+  config.set(
+    Object.assign(baseConfig, ENV === "production" ? prodSpecificConfig : devSpecificConfig)
+  );
 };
--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -6,17 +6,45 @@ import {todosReducer} from "./todos/todos.store";
 import {Todo} from "./todos/todo.model";
 import {watchTimeReducer} from "./+lazy-test/lazy-test.store";
 
+/**
+ * This interface describes the relevant "state" of your application,
+ * or at least what you need to restore it properly.
+ * When you inject a {Store} to any of your structures, it will always be a
+ * {Store<AppState>} (as long as you follow our convention, obviously).
+ * To access a particular part of your state, you might then just call
+ * `.select(state => state.{whatever})`. This will return an {Observable<T>},
+ * where {T} is the type of the partial state you selected. You might combine these
+ * as well.
+ */
 export interface AppState {
   todos: List<Todo>;
   watchTime: number;
 }
 
-export const reducers = {
-  todos: todosReducer,
+/**
+ * These reducers in this object refer to the {AppState} mentioned above.
+ * Our conventions - which is also preferred by most projects out there - is:
+ * - There should be exactly one reducer per entry in your {AppState}. This optimizes usability and maintainability.
+ * - The reducers registered here should be listed with the same key as the corresponding entry in {AppState} has.
+ *   This simplifies relation management.
+ */
+const reducers = {
+  todos:     todosReducer,
   watchTime: watchTimeReducer
 };
 
 // Generate a reducer to set the root state in dev mode for HMR
+
+/**
+ * This reducer is used to provide the ability to set the "root state" with a single call,
+ * i.e. the payload forwarded to this function contains the whole {AppState}.
+ * While this is not useful in general, it is essentially required for proper HMR, which is used
+ * in development mode.
+ * It will be composed with the reducers listed above.
+ * @param reducer The inner reducer, which will receive the state and action values in case the
+ *                outer reducer does not consume the action - i.e. in every case apart from SET_ROOT_STATE.
+ * @returns {(state:any, action:Action)=>(any|any)}
+ */
 function stateSetter(reducer: ActionReducer<any>): ActionReducer<any> {
   "use strict";
   return function (state: any, action: Action) {
@@ -27,10 +55,30 @@ function stateSetter(reducer: ActionReducer<any>): ActionReducer<any> {
   };
 }
 
-const DEV_REDUCERS                                         = [stateSetter];
+// Feel free to add more reducers for development mode to this list, e.g. https://github.com/ngrx/store-log-monitor.
+const DEV_REDUCERS = [stateSetter];
+// This reducer is only used in development mode and is the result of a composition of DEV_REDUCERS and
+// the reducers that are related to your {AppState}.
 const developmentReducer: (state: any, action: any) => any = compose(...DEV_REDUCERS, combineReducers)(reducers);
-// TODO: Use and eval the ENV variable (via DefinePlugin) to properly define a production reducer.
+// This reducer is only used in production mode and is just a combination of the once your provided for your {AppState}.
+const productionReducer: (state: any, action: any) => any  = combineReducers(reducers);
 
+/**
+ * Regularly, the "reducer" may a function value. However, due to AoT restrictions,
+ * we have to explicitly provide a function here.
+ * @param state The current state.
+ * @param action The action to evaluate.
+ * @returns {any} The result state.
+ */
 export function rootReducer(state: any, action: any): any {
-  return developmentReducer(state, action);
+  // It might seem ineffective to evaluate this statement multiple times.
+  // Don't worry: "ENV" is provided by webpack's DefinePlugin, so it is treated as a constant value.
+  // Thus, the result of this expression is also constant.
+  // As a result, webpack can use it as a condition to drop some parts of the code w.r.t. to the result
+  // of this expression.
+  if (ENV !== "production") {
+    return developmentReducer(state, action);
+  } else {
+    return productionReducer(state, action);
+  }
 }
