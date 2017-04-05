92397aa1cc6469b9ef977f0754aaa2a254e7f6e3
DorianGrey <ShadowVampire@web.de>
2017-04-05 08:55:40 +0200

    (cleanup): Remove no longer required workaround for enabling the angular debug tools

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 // Angular 2
 import {enableDebugTools} from "@angular/platform-browser";
-import {enableProdMode, ApplicationRef} from "@angular/core";
+import {enableProdMode} from "@angular/core";
 
 // Angular debug tools in the dev console
 // https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
@@ -15,13 +15,25 @@ if ("production" === ENV) {
 } else {
 
   _decorateModuleRef = (modRef: any) => {
-    const appRef = modRef.injector.get(ApplicationRef);
-    const cmpRef = appRef.components[0];
+    /*
+      Note: In earlier versions, it was required to pick a reference to the
+      `window.ng` token for properly adding the debug tools without getting them overwritten
+      by angular.
 
-    let _ng = (<any>window).ng;
-    enableDebugTools(cmpRef);
-    (<any>window).ng.probe      = _ng.probe;
-    (<any>window).ng.coreTokens = _ng.coreTokens;
+         const appRef = modRef.injector.get(ApplicationRef);
+         const cmpRef = appRef.components[0];
+
+         let _ng = (<any>window).ng;
+         enableDebugTools(cmpRef);
+         (<any>window).ng.probe      = _ng.probe;
+         (<any>window).ng.coreTokens = _ng.coreTokens;
+
+      This was tracked in https://github.com/angular/angular/issues/12002.
+      Since the corresponding pull request https://github.com/angular/angular/pull/12003 is merged,
+      this workaround is no longer required.
+     */
+    enableDebugTools(modRef);
+
     return modRef;
   };
 }
